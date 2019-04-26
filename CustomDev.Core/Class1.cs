c140f7b1bbc3241e8723143f39db11d4763266b8
François DORIN <francois@custom-dev.fr>
2019-04-26 08:33:13 +0200

    commit

--- CustomDev.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CustomDev.Core
-{
-    public class Class1
-    {
-    }
-}
---
+++ CustomDev.Core/ExceptionExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace CustomDev
+{
+    public static class ExceptionExtension
+    {
+        public static string ToFullTrace(this Exception ex)
+        {
+            StringBuilder builder = new StringBuilder();
+            Exception currentException = ex;
+
+            builder.AppendLine("=== BEGIN EXCEPTION ===");
+
+            while (ex != null)
+            {
+                builder.AppendLine(ex.GetType().ToString());
+                builder.AppendLine(ex.Message);
+                builder.AppendLine(ex.StackTrace);
+                builder.AppendLine();
+                ex = ex.InnerException;
+            }
+
+            builder.AppendLine("=== END EXCEPTION ===");
+
+            return builder.ToString();
+        }
+    }
+}
